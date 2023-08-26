764978813f8b3b7c96880b913957cf9d755b1f5f
Muhammad Hamza Farrukh <31820096+hamza24farrukh@users.noreply.github.com>
2023-08-26 21:24:51 +0500

    Add files via upload

---
+++ ex_1.py
@@ -0,0 +1,10 @@
+# A Good First Program
+
+print("hello world")
+print("hello again")
+print("i like typing this.")
+print("this is fun")
+print("Yay! printing.")
+print("i'd be much rather you 'not'")
+print('i "said" donot touch this')
+
